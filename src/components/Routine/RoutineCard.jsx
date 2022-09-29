a3bf0345559e23c2c2f53bd975e6d05fc84dd41a
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-09-30 00:48:02 +0300

    style refactor

--- src/components/Routine/RoutineCard.jsx
+++ src/components/Routine/RoutineCard.jsx
@@ -1,67 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RoutineCard = ({ name, dayOrder, exercises }) => {
   console.log(exercises);
   return (
-    <table className="bg-white min-w-full">
-      <thead>
-        <tr>
-          <th
-            colSpan={3}
-            className="text-xl border border-black bg-amber-500 text-center"
-          >
-            &nbsp;{`day-${dayOrder}`}
-          </th>
-        </tr>
-        <tr>
-          <th
-            colSpan={3}
-            className="text-xl border border-black bg-white text-center"
-          >
-            &nbsp;{name}
-          </th>
-        </tr>
+    <div className="flex flex-col">
+      <table className="bg-white h-24">
+        <thead>
+          <tr>
+            <th
+              colSpan={3}
+              className="text-xl border border-black bg-amber-500 text-center"
+            >
+              &nbsp;{`day-${dayOrder}`}
+            </th>
+          </tr>
+          <tr>
+            <th
+              colSpan={3}
+              className="text-xl border border-black bg-white text-center"
+            >
+              &nbsp;{name}
+            </th>
+          </tr>
+        </thead>
+      </table>
+      <table className="bg-white h-12">
         <tr>
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
             Exercise Name
           </th>
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Sets
+            Weight
           </th>
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
             Reps
           </th>
         </tr>
-      </thead>
-      <tbody className="">
-        {exercises?.map((e) => (
-          <tr>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <div className="flex items-center">
+        {exercises?.map((s) => (
+          <tr key={s.id} className="">
+            <td className="px-6 h-12  md:whitespace-nowrap">
+              <div className="flex items-center overflow-hidden">
                 <img
                   className="h-12 w-12 rounded-full"
-                  alt={`exercise ${e.name} gif`}
-                  src={e.gifLink}
+                  alt={`exercise ${s.exercise?.name} gif`}
+                  src={s.exercise?.gifLink}
                 />
-                {e.name}
+                <Link
+                  className="hover:underline"
+                  to={`/exercises/${s.exercise?.id}`}
+                >
+                  {s.exercise?.name}
+                </Link>
               </div>
             </td>
-            <td className="px-6 py-4 whitespace-nowrap">{e.name}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{e.name}</td>
+            <td className="px-6 py-4 h-12 md:whitespace-nowrap">{s.weight}</td>
+            <td className="px-6 py-4 h-12 md:whitespace-nowrap">
+              {s.numberOfReps}
+            </td>
           </tr>
         ))}
-        {/* <div>
-            <div className="flex">
-              <button class="bg-white hover:bg-gray-100 flex-grow text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                Edit Routine
-              </button>
-              <button class="bg-red-600 hover:bg-red-500 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                Delete
-              </button>
-            </div>
-          </div> */}
-      </tbody>
-    </table>
+      </table>
+    </div>
   );
 };
 
