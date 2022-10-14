e163d7ee18e2772a1df4e9b9340b23b10407448e
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-10-14 19:45:51 +0300

    added progress page

--- src/pages/Progress/Progress.jsx
+++ src/pages/Progress/Progress.jsx
@@ -1,10 +1,74 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useEffect } from "react";
+import DoughnutChart from "../../components/Progress/DoughnutChart";
+import StatsTable from "../../components/Progress/StatsTable";
+import TopExercisesTable from "../../components/Progress/TopExercisesTable";
+import TopWorkoutPlansTable from "../../components/Progress/TopWorkoutPlansTable";
 import { AuthContext } from "../../context/AuthContext";
-import exercisesService from "../../services/exercises";
+import analyticsService from "../../services/analytics";
 
 const Progress = () => {
-  return <div>Progress</div>;
+  const [exercisesByMuscleGroupData, setExercisesByMuscleGroupData] = useState(
+    {}
+  );
+  const [statsData, setStatsData] = useState({});
+  const [topFiveExercises, setTopFiveExercises] = useState([]);
+  const [topThreeWorkoutPlans, setTopThreeWorkoutPlans] = useState([]);
+  const {
+    user: { userId, token },
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getExercisesCountByMuscleGroup = async () => {
+      const data = await analyticsService.getExercisesCountByMuscleGroup(
+        userId,
+        token
+      );
+      setExercisesByMuscleGroupData(data);
+    };
+    const getTopFiveUsedExercises = async () => {
+      const data = await analyticsService.getTopUsedExercises(userId, 5, token);
+      setTopFiveExercises(data);
+    };
+    const getStatsData = async () => {
+      const data = await analyticsService.getAllTimeStats(userId, token);
+      setStatsData(data);
+    };
+    const getTopThreeUsedWorkoutPlans = async () => {
+      const data = await analyticsService.getTopUsedWorkoutPlans(
+        userId,
+        3,
+        token
+      );
+      setTopThreeWorkoutPlans(data);
+    };
+    getTopThreeUsedWorkoutPlans();
+    getTopFiveUsedExercises();
+    getExercisesCountByMuscleGroup();
+    getStatsData();
+  }, []);
+
+  return (
+    <div className="flex">
+      <div className="max-w-[20vw]">
+        <DoughnutChart
+          labels={Object.keys(exercisesByMuscleGroupData)}
+          values={Object.values(exercisesByMuscleGroupData)}
+        />
+      </div>
+      <div className="flex gap-2">
+        <div>
+          <TopExercisesTable topExercises={topFiveExercises} />
+        </div>
+        <div>
+          <TopWorkoutPlansTable topWorkoutPlans={topThreeWorkoutPlans} />
+        </div>
+        <div>
+          <StatsTable stats={statsData} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Progress;
