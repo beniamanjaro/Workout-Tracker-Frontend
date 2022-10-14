d6dffc81b0f3a5d064b9962fa8b44b0c3808ac95
Benjamin Vrajitor <benjamin.vrajitor@amdaris.com>
2022-10-14 19:49:24 +0300

    added completed routine card

---
+++ src/components/CompletedRoutine/CompletedRoutineCard.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CompletedRoutineCard = ({ name, exercises }) => {
+  return (
+    <div className="flex flex-col">
+      <table className="bg-white h-24">
+        <thead>
+          <tr>
+            <th
+              colSpan={3}
+              className="text-xl border border-black bg-amber-500 text-center"
+            >
+              &nbsp;{`day-${0}`}
+            </th>
+          </tr>
+          <tr>
+            <th
+              colSpan={3}
+              className="text-xl border border-black bg-white text-center"
+            >
+              &nbsp;{name}
+            </th>
+          </tr>
+        </thead>
+      </table>
+      <table className="bg-white h-12 w-full">
+        <tbody>
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Exercise Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Weight
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Reps
+            </th>
+          </tr>
+          {exercises?.map((s) => (
+            <tr key={s.id} className="">
+              <td className="px-6 h-12">
+                <div className="flex items-center justify-start overflow-hidden">
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    alt={`exercise ${s.exercise?.name} gif`}
+                    src={s.exercise?.gifLink}
+                  />
+                  <Link
+                    className="hover:underline"
+                    to={`/exercises/${s.exercise?.id}`}
+                  >
+                    {s.exercise?.name}
+                  </Link>
+                </div>
+              </td>
+              <td className="px-6 py-4 h-12 md:whitespace-nowrap">
+                {s.weight}
+              </td>
+              <td className="px-6 py-4 h-12 md:whitespace-nowrap">{s.reps}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CompletedRoutineCard;
